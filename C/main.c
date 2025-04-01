0525dbe9250e1867889ddca493c10fe0a7809515
Pierce Neupane <sahasneupane85@gmail.com>
2025-04-01 16:19:45 +0545

    added todo in C

--- C/main.c
+++ C/main.c
@@ -1,20 +1,13 @@
 #include <stdio.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 #define PORT 6969
 
-int main(int argc, char *argv[]) {
-  if (argc != 0) {
-    /* printf("Hello %s\n", argv[1]); */
-    return 0;
-  }
+int main() {
 
-  // protocol number is defined in IANA website
-  // HOPOPT = 0
-  // IPv4 = 4
-  // TCP = 6
-  // UDP = 17
-  // IPv6 = 41
   int sockfd = socket(AF_INET, SOCK_STREAM, 0);
   if (sockfd == -1) {
     perror("socket faield");
@@ -22,15 +15,46 @@ int main(int argc, char *argv[]) {
   }
 
   // BIND
-  struct sockaddr_in addr;
-
-  sin_addr.s_addr = sizeof(addr);
-  addr.sin_family = AF_INET;
-  addr.sin_port = htons(PORT);
+  struct sockaddr_in server_addr;
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(PORT);
   // htonl -> host to network long
-  addr.sin_addr.s_addr = htonl(INADDR_ANY); // sin_addr is ip address of socket
+  server_addr.sin_addr.s_addr = htonl(INADDR_ANY); // sin_addr is ip address of socket
 
-  bind(sockfd, (struct sockaddr*)&addr, sizeof(addr));
+  if (bind(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) { // assigns an address to socket
+    perror("Bind failed");
+    close(sockfd);
+    exit(EXIT_FAILURE);
+  }
 
+  // listen
+  if (listen(sockfd, 5)) {
+    perror("Listening Failed");
+    close(sockfd);
+    exit(EXIT_FAILURE);
+  }
+
+  // accept
+  int client_fd;
+  struct sockaddr_in client_addr;
+  socklen_t client_len = sizeof(client_addr); // 32 bit integer
+
+  client_fd = accept(sockfd, (struct sockaddr *) &client_addr, &client_len);
+  if (client_fd < 0) {
+    perror("Accept Failed");
+    close(sockfd);
+    exit(EXIT_FAILURE);
+  }
+  printf("Client Connected!\n");
+
+  // receive from client recv()
+  char buffer[1024] = {0};
+
+  // send to client send()
+  char msg[1024] = "hello from server";
+  
+  // multiple clients
+  // 1. use multithreading or select()
+  close(sockfd);
   return 0;
 }
